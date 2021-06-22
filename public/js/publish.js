e50f72a974227266cac9e2cb8888fa4bebe96a7a
yohannlc <yohann.lecamx@gmail.com>
2021-06-22 13:13:18 +0200

    test connect publish page

---
+++ public/js/publish.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    if (!Cookies.exists("login") && !Cookies.exists("token")) {
+
+        var elems = document.getElementsByClassName('input-text');
+        for (var i=0 ; i<elems.length ; i+=1){
+            elems[i].style.opacity = "0.5";
+            elems[i].disabled = "true";
+        }
+
+        document.getElementById("publish-trip").style.action = "signup.html";
+
+        publish_button = document.getElementById("publish");
+        publish_button.innerHTML = "Se connecter";
+        publish_button.type = "button";
+        publish_button.setAttribute('onclick',"window.location.href='signin.html';");
+
+        info = document.getElementById("info");
+        info.style.display = "none";
+       
+        no_connect = document.getElementById("no-connect");
+        no_connect.style.display = "flex";
+        
+    }
+
+});
