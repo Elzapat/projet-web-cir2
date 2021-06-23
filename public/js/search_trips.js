cbf9f0a31e3554cc84cc1ce851b79b2affa7451b
yohannlc <yohann.lecamx@gmail.com>
2021-06-23 16:38:09 +0200

    valid button modified

--- public/js/search_trips.js
+++ public/js/search_trips.js
@@ -159,8 +159,7 @@ function display_trips_results(search_city, search_isen, search_date, trips) {
                     data-price="${trip.prix}"
                     data-trip-id="${trip.id_trajet}"
                     class="choice"
-                >
-                </span>
+                ></span>
                     Choisir
                 </button>
             </div>
