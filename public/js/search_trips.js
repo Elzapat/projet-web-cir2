2019eb3ba6fa2eea8c220f37cbca05fc5dc98e61
yohannlc <yohann.lecamx@gmail.com>
2021-06-23 14:37:04 +0200

    not connected publish page managed

--- public/js/search_trips.js
+++ public/js/search_trips.js
@@ -121,7 +121,7 @@ function display_trips_results(search_city, search_isen, search_date, trips) {
                 <img id="arrow" src="images/fleches.png">
                 <a id="end-loc-res" class="brut-input">${end}</a>
                 <a id="date-search" class="brut-input">${search_date}</a>
-                <a id="nb-results" class="brut-input">${nb_results}</a>
+                <a id="nb-results" class="brut-input">${nb_results} résultat${nb_results > 1 ? 's' : ''}</a>
             </div>
         </section>
     `;
@@ -149,10 +149,74 @@ function display_trips_results(search_city, search_isen, search_date, trips) {
                 <span class="price">Prix : ${trip.prix}€</span>
                 <span class="seats">${nb_places} place${nb_places > 1 ? 's' : ''}</span>
                 <span class="driver">par ${trip.pseudo}</span>
-                <button class="choice">Choisir</button>
+                <button
+                    data-start-loc="${depart}"
+                    data-end-loc="${arrivee}"
+                    data-start-time="${heure_depart}"
+                    data-end-time="${heure_arrivee}"
+                    data-price="${trip.prix}"
+                    class="choice"
+                >
+                    Choisir
+                </button>
             </div>
         `;
     }
+
+    // Validation page when choosing a trip
+    for (let button of document.getElementsByClassName("choice")) {
+        if (Cookies.exists("login") && Cookies.exists("token")) {
+            button.addEventListener("click", event => {
+                display_validation_page(event.target);
+            });
+        } else {
+            document.getElementById("choise-button").innerHTML = "Se connecter";
+            button.addEventListener("click", event => {
+                button.setAttribute('onclick',"window.location.href='signin.html';");
+            });
+        }
+    }
+
+}
+
+function display_validation_page(button) {
+    document.getElementById("search-trips-results").style.transform = "translateX(-100vw)";
+
+    setTimeout(() => {
+        document.getElementsByTagName("main")[0].innerHTML = `
+            <section id="validation">
+                <div class="publish-row">
+                    <a  class="text-valid">
+                        <svg id="logo" class="link-icon" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="car" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-car fa-w-16 fa-9x"><path fill="currentColor" d="M499.99 176h-59.87l-16.64-41.6C406.38 91.63 365.57 64 319.5 64h-127c-46.06 0-86.88 27.63-103.99 70.4L71.87 176H12.01C4.2 176-1.53 183.34.37 190.91l6 24C7.7 220.25 12.5 224 18.01 224h20.07C24.65 235.73 16 252.78 16 272v48c0 16.12 6.16 30.67 16 41.93V416c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32v-32h256v32c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32v-54.07c9.84-11.25 16-25.8 16-41.93v-48c0-19.22-8.65-36.27-22.07-48H494c5.51 0 10.31-3.75 11.64-9.09l6-24c1.89-7.57-3.84-14.91-11.65-14.91zm-352.06-17.83c7.29-18.22 24.94-30.17 44.57-30.17h127c19.63 0 37.28 11.95 44.57 30.17L384 208H128l19.93-49.83zM96 319.8c-19.2 0-32-12.76-32-31.9S76.8 256 96 256s48 28.71 48 47.85-28.8 15.95-48 15.95zm320 0c-19.2 0-48 3.19-48-15.95S396.8 256 416 256s32 12.76 32 31.9-12.8 31.9-32 31.9z" class=""></path></svg>
+                    </a>
+                    <a class="text-valid">${button.dataset.price}</a>
+                </div>
+                <div class="publish-row">
+                    <div id="loc">
+                        <div class="publish-row text-valid">
+                            <div>
+                                <a>${button.dataset.startLoc}</a><br>
+                                <a>${button.dataset.startTime}</a>
+                            </div>
+                            <div>
+                                <a>${button.dataset.endLoc}</a><br>
+                                <a>${button.dataset.endTime}</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="publish-row">
+                    <a class="text-valid">5,00€</a>
+                </div>
+                <div class="publish-row">
+                    <button>Valider</button>
+                </div>
+                <div class="publish-row">
+                    <button>Télécharger le récapitulatif</button>
+                </div>
+            </section>
+        `;
+    }, 500);
 }
 
 function prettier_time(time) {
@@ -160,4 +224,3 @@ function prettier_time(time) {
     hours_mins.pop();
     return hours_mins.join('h');
 }
-
